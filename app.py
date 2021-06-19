5ea5339fd15274ea16ecf87eb5d5e0aea004d9c6
edenbartov <68859419+edenbartov@users.noreply.github.com>
2021-06-19 16:25:36 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,111 @@
+import redis
+import json
+import xxhash
+from datetime import datetime
+from flask import Flask, request
+import requests
+import boto3
+import threading
+import socket
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('living_nodes',endpoint_url="http://localhost:8000")
+cache = {}
+app = Flask(__name__)
+delay_period = 15
+last = 0 
+
+def signal_alive():
+    timestamp = str(datetime.now())
+    ip = socket.gethostbyname(socket.gethostname())
+    print(timestamp)
+    item = {'ip': ip,
+            'lastAlive': timestamp
+            }
+    table.put_item(Item=item)
+
+def get_live_node_list():
+    # response = table.scan()
+    now = datetime.now()
+    past_periond = now - datetime.timedelta(seconds=delay_period)
+     response = table.query(
+        KeyConditionExpression=Key('lastAlive').between(str(past_periond), str(now))
+    )
+    return (x['ip'] for x in response['items'])
+
+
+
+def get_nodes(key):
+    nodes = get_live_node_list()
+    temp_key = xxhash.xxh64_intdigest(key) % 1024
+    node = nodes[(temp_key % len(nodes))]
+    alt_node = nodes[((temp_key + 1) % len(nodes))]
+    return node, alt_node
+
+def get_url(node, key, op, data=None, expiration_date=None):
+    if op == 'put':
+        return f'http://{node}:5000/{op}_internaly?str_key={key}&data={data}&expiration_date={expiration_date}'
+    else:
+        return f'http://{node}:5000/{op}_internaly?str_key={key}'
+
+
+@app.route('/put', methods=['GET', 'POST'])
+def put():
+    key = request.args.get('str_key')
+    data = request.args.get('data')
+    expiration_date = request.args.get('expiration_date')
+    
+    node, alt_node =  get_nodes(key)
+
+    try:
+        ans = requests.post(get_url(node,key,'put',data,expiration_date,))
+        ans = requests.post(get_url(alt_node,key,'put',data,expiration_date,))
+    except:
+        return json.dumps({'status_code': 404}).json()
+    
+    return ans.json()
+
+#  get items from nodes
+@app.route('/get', methods=['GET', 'POST'])
+def get():
+    key = request.args.get('str_key')
+
+    node, alt_node = get_nodes(key)
+    
+    try:
+        ans = requests.get(get_url(node,key,'get'))
+    except:
+        try:
+            ans = requests.get(get_url(alt_node,key,'get'))
+        except requests.exceptions.ConnectionError:
+            return ans
+    return ans.json().get('item')
+
+
+@app.route('/put_internaly', methods=['GET', 'POST'])
+def put_internaly():
+    key = request.args.get('str_key')
+    data = request.args.get('data')
+    expiration_date = request.args.get('expiration_date')
+    #actually seting the data
+    cache[key] = (data, expiration_date)
+    print(cache)
+    return json.dumps({'status code': 200,
+                       'item': cache[key]})
+
+@app.route('/get_internaly', methods=['GET', 'POST'])
+def get_internaly():
+    key = request.args.get('str_key')
+    # getting the data out of the cache
+    item = cache[key]
+    response = json.dumps({'status code': 200,
+                           'item': item[0]})
+    return response
+
+now = datetime.now()
+if (now.second> (last +10)%60):
+    last  =  now.second
+    signal_alive()
+
+if __name__ == '__main__':
+    app.run()
---
+++ deploy.sh
@@ -0,0 +1,83 @@
+KEY_NAME="Eden-Yaron-`date +'%F'`"
+KEY_PEM="$KEY_NAME.pem"
+
+echo "create key pair $KEY_PEM to connect to instances and save locally"
+aws ec2 create-key-pair --key-name $KEY_NAME  --query "KeyMaterial" --output text > $KEY_PEM
+# secure the key pair
+chmod 600 $KEY_PEM
+
+# figure out my ip
+echo "getting my ip"
+MY_IP=$(curl ipinfo.io/ip)
+echo "My IP: $MY_IP"
+
+
+# get subnets for the ELB and vpc id
+echo "getting all subnets and vpc id's"
+SUB_ID_1=$(aws ec2 describe-subnets --filters Name=default-for-az,Values=true | jq -r .Subnets[0] | jq -r .SubnetId)
+SUB_ID_2=$(aws ec2 describe-subnets --filters Name=default-for-az,Values=true | jq -r .Subnets[1] | jq -r .SubnetId)
+SUB_ID_3=$(aws ec2 describe-subnets --filters Name=default-for-az,Values=true | jq -r .Subnets[2] | jq -r .SubnetId)
+SUB_ID_4=$(aws ec2 describe-subnets --filters Name=default-for-az,Values=true | jq -r .Subnets[3] | jq -r .SubnetId)
+VPC_ID=$(aws ec2 describe-subnets --filters Name=default-for-az,Values=true | jq -r .Subnets[0] | jq -r .VpcId)
+VPC_CIDR_BLOCK=$(aws ec2 describe-vpcs --filters Name=vpc-id,Values=$VPC_ID | jq -r .Vpcs[0].CidrBlock)
+echo $SUB_ID_1
+echo $SUB_ID_2
+echo $SUB_ID_3
+echo $SUB_ID_4
+echo $VPC_ID
+echo $VPC_CIDR_BLOCK
+
+echo "createing stack yaron-eden stack"
+STACK_RES=$(aws cloudformation create-stack --stack-name yaron-eden-stack --template-body file://ec2CloudFormation.yml --capabilities CAPABILITY_IAM \
+	--parameters ParameterKey=InstanceType,ParameterValue=t2.micro \
+	ParameterKey=KeyName,ParameterValue=$KEY_NAME \
+	ParameterKey=SSHLocation,ParameterValue=$MY_IP/32 \
+	ParameterKey=SubNetId1,ParameterValue=$SUB_ID_1 \
+	ParameterKey=SubNetId2,ParameterValue=$SUB_ID_2 \
+	ParameterKey=SubNetId3,ParameterValue=$SUB_ID_3 \
+	ParameterKey=SubNetId4,ParameterValue=$SUB_ID_4 \
+	ParameterKey=VPCId,ParameterValue=$VPC_ID \
+	ParameterKey=VPCcidr,ParameterValue=$VPC_CIDR_BLOCK)
+
+echo "waiting for stack yaron-eden-stack to be created"
+STACK_ID=$(echo $STACK_RES | jq -r '.StackId')
+aws cloudformation wait stack-create-complete --stack-name $STACK_ID
+
+REGION=us-east-1
+
+# get the wanted stack 
+STACK=$(aws cloudformation --region $REGION describe-stacks --stack-name yaron-eden-stack | jq -r .Stacks[0])
+# stack outputs
+echo "printing stack outputs"
+OUTPUTS=$(echo $STACK | jq -r .Outputs)
+echo $OUTPUTS
+
+echo "getting instances IP"
+PUBLIC_IP_1=$(aws cloudformation --region $REGION describe-stacks --stack-name yaron-eden-stack --query "Stacks[0].Outputs[?OutputKey=='Instance1IP'].OutputValue" --output text)
+PUBLIC_IP_2=$(aws cloudformation --region $REGION describe-stacks --stack-name yaron-eden-stack --query "Stacks[0].Outputs[?OutputKey=='Instance2IP'].OutputValue" --output text)
+PUBLIC_IP_3=$(aws cloudformation --region $REGION describe-stacks --stack-name yaron-eden-stack --query "Stacks[0].Outputs[?OutputKey=='Instance3IP'].OutputValue" --output text)
+PUBLIC_IP_4=$(aws cloudformation --region $REGION describe-stacks --stack-name yaron-eden-stack --query "Stacks[0].Outputs[?OutputKey=='Instance4IP'].OutputValue" --output text)
+
+ssh-add ./$KEY_PEM
+
+echo "uploding code to instances"
+scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" app.py ubuntu@$PUBLIC_IP_1:/home/ubuntu/
+scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" app.py ubuntu@$PUBLIC_IP_2:/home/ubuntu/
+scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" app.py ubuntu@$PUBLIC_IP_3:/home/ubuntu/
+scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" app.py ubuntu@$PUBLIC_IP_4:/home/ubuntu/
+echo "done uploading"
+
+echo "running the code"
+
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 <<EOF
+    sudo apt-get update
+    sudo apt-get install python3-pip -y
+    sudo apt-get install python3-flask -y
+    # run app
+    nohup flask run --host 0.0.0.0  &>/dev/null &
+    exit
+EOF
+
+
+echo "done"
+
