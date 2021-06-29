5d1da0ab9edb4d5acde3c9a7417ec4a816cf6877
edenbartov <68859419+edenbartov@users.noreply.github.com>
2021-06-29 21:31:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
 table = dynamodb.Table('LivingNodes')
 cache = {}
 app = Flask(__name__)
-delay_period = 30 * 1000  # 30 seconds in milis
+delay_period = 12 * 1000  # 12 seconds in milis
 last = 0
 ip_address = ""
 live_nodes_pool_size = 1
@@ -45,11 +45,11 @@ def repartition(current_num_nodes):
     for v_key in cache:
         new_node_index = jump.hash(int(v_key), current_num_nodes)
         old_node_index = jump.hash(int(v_key), live_nodes_pool_size)
+        nodes = get_live_node_list()
         # need to send all the data to the new node
-        if new_node_index != old_node_index:
+        if nodes[new_node_index] != nodes[old_node_index]:
             bucket = cache.pop(v_key)
             # alt_node = jump.hash((v_key+1) % 1024, current_num_nodes)
-            nodes = get_live_node_list()
             node = nodes[new_node_index]
             alt_node = nodes[(new_node_index + 1) % current_num_nodes]
             for key in bucket:
@@ -58,7 +58,6 @@ def repartition(current_num_nodes):
                     put_data(key, data, expiration_date, v_key, node, alt_node)
                 except:
                     continue
-
 
     live_nodes_pool_size = current_num_nodes
 
@@ -92,14 +91,14 @@ def get_nodes(key):
         index = jump.hash(v_key, len(nodes))
         node = nodes[index]
         # alt_node = nodes[jump.hash((v_key + 1) % 1024, len(nodes))]
-        alt_node = nodes[(index + 1)% len(nodes)]
+        alt_node = nodes[(index + 1) % len(nodes)]
         return v_key, node, alt_node
     except Exception as e:
         app.logger.info(f'failed in the get_nodes {e}')
         return None
 
 
-def get_url(node, key, op, v_key,data=None, expiration_date=None):
+def get_url(node, key, op, v_key, data=None, expiration_date=None):
     if op == 'put':
         return f'http://{node}:8080/{op}_internaly?v_key={v_key}&str_key={key}&data={data}' \
                f'&expiration_date={expiration_date}'
@@ -181,6 +180,7 @@ def get_internaly():
         response = json.dumps({'status code': 404,
                                'item': "item does not exists"})
     return response
+
 
 if __name__ == '__main__':
     ip_address = requests.get('https://api.ipify.org').text
